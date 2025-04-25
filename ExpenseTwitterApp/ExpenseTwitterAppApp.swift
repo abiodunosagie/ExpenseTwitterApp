ff852bd9d2334e43e538b8b71d1cc780d21e3004
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-25 22:08:08 +0100

    Initial Commit

---
+++ ExpenseTwitterApp/ExpenseTwitterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTwitterAppApp.swift
+//  ExpenseTwitterApp
+//
+//  Created by Abiodun Osagie on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTwitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WalletView()
+        }
+    }
+}
---
+++ ExpenseTwitterApp/Screens/WalletView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTwitterApp
+//
+//  Created by Abiodun Osagie on 25/04/2025.
+//
+
+import SwiftUI
+
+struct WalletView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WalletView()
+}
