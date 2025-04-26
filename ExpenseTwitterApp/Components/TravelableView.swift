1c2d0aab4e33b903222515161543a92bd9896826
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-26 10:57:28 +0100

    Travelalbe

---
+++ ExpenseTwitterApp/Components/TravelableView.swift
@@ -0,0 +1,18 @@
+//
+//  TravelableView.swift
+//  ExpenseTwitterApp
+//
+//  Created by Abiodun Osagie on 26/04/2025.
+//
+
+import SwiftUI
+
+struct TravelableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TravelableView()
+}
